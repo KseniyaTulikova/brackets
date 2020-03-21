6173a532687fb05095924138f3b47914b6f50cb7
Kseniya Tulikava <ksenia77383@gmail.com>
2020-03-21 21:03:43 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let currentStr = str;
+    let lengthOfStr = 0; //should be if result is true
+
+        const removePairOfBrackets = () => {
+            lengthOfStr = currentStr.length;
+            bracketsConfig.forEach(pair => {
+                currentStr = currentStr.replace(pair.join(''), '');
+            });
+
+            return (lengthOfStr !== currentStr.length) ? removePairOfBrackets() : (currentStr.length == 0);
+        }
+
+        return removePairOfBrackets();
+  }
+
+
